e3337534dbd8d7e886dfa3a972bd84cf2246fa50
moony564 <71317753+moony564@users.noreply.github.com>
2022-02-03 14:37:47 +0900

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -56,13 +56,18 @@ async def 유튜브(ctx):
 @app.command()
 async def 장비(ctx):
     embed = discord.Embed(colour = 0x00ff00)
-    embed.add_field(name='```[접두사]```', value='```!```', inline=False)
+    embed.add_field(name='```[삼국지]```', value='```장비```', inline=False)
     embed.add_field(name='마우스', value='G402', inline=False)
     embed.add_field(name='키보드', value='G610K V2', inline=False)
     embed.add_field(name='그래픽카드', value='NVIDIA GeForce GTX 1650', inline=False)
     embed.add_field(name='CPU', value='AMD Ryzen 5 5600X', inline=False)
     embed.add_field(name='헤드셋', value='ABKO HACKER N550 ENC 가상 7.1 RGB 3D 진동 헤드셋', inline=False)
     await ctx.send(embed=embed)
+    
+@app.command()
+async def 살려줘(ctx):
+    await ctx.reply('여기서 1년만 하면 꺼내주신다고 하셨는데..')
+    await ctx.reply('10년이나 같혀있다..')
     
 access_token = os.environ["BOT_TOKEN"]
 app.run(access_token)
