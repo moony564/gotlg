b0c83f703b3dfe29f63012adb51837b2d9402bb3
moony564 <71317753+moony564@users.noreply.github.com>
2022-02-03 12:34:00 +0900

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -1,100 +1,63 @@
 import discord
+from discord import message
+from discord import embeds
+from discord import colour
+from discord.ext import commands
+import time
 import os
 
-client = discord.Client()
+app = commands.Bot(command_prefix='!')
 
-@client.event
+@app.event
 async def on_ready():
-
-    print(client.user.name)
+    print(app.user.name)
     print("start")
-    game = discord.Game('/명령어')
-    await client.change_presence(status=discord.Status.online, activity=game)
+    game = discord.Game('!명령어')
+    await app.change_presence(status=discord.Status.online, activity=game)
 
+@app.command()
+async def 명령어(ctx):
+    embed = discord.Embed(colour = 0x00ff00)
+    embed.add_field(name='```접두사```', value='```!```', inline=False)
+    embed.add_field(name='1', value='나이', inline=False)
+    embed.add_field(name='2', value='혈액형', inline=False)
+    embed.add_field(name='3', value='사는곳', inline=False)
+    embed.add_field(name='4', value='방송계기', inline=False)
+    embed.add_field(name='5', value='트위치', inline=False)
+    embed.add_field(name='6', value='유튜브', inline=False)
+    embed.add_field(name='7', value='디코', inline=False)
+    await ctx.send(embed=embed)
 
-@client.event
-async def on_message(message):
-    if "시발" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
+# embed.add_field(value='', inline=False) #임배드
 
-    if "시발" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
+@app.command()
+async def 나이(ctx):
+    await ctx.send('09년생')
 
-    if "씨발" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
+@app.command()
+async def 혈액형(ctx):
+    await ctx.send('O형')
 
-    if "tlqkf" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "Tlqkf" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "미친" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "병신" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "지랄" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "존나" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "존1나" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "ㅈㄴ" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if "ㅅㅂ" in message.content:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 욕을 사용하셨습니다.")
-
-    if message.content == "!봇정보":
-        await message.channel.send("만든사람 : 마딩")
-        await message.channel.send("버전 : 1.0v")
-        await message.channel.send("개발자의 한마디 : 이하린TV수준이 뛰떨어지고 시궁창 인생")
-
-    if message.content == "/명령어":
-        await message.channel.send("https://moonytv.neocities.org/갓엘지/도움말.html + !봇정보")
-
-    if "바보지?" in message.content:
-        await message.channel.send("당연하지!")
-
-    if message.content == "!나이":
-        await message.channel.send("09년생")
-
-    if message.content == "!혈액형":
-        await message.channel.send("O형")
-
-    if message.content == "!사는곳":
-        await message.channel.send("대한민국")
-
-    if message.content == "!방송계기":
-        await message.channel.send("다른 스트리머가 게임하면서 방송하는게 멋있어서")
-
-    if message.content == "!유튜브":
-        await message.channel.send("https://www.youtube.com/channel/UCXPOGWwjH-noqWCn_URbWGA")
-
-    if message.content == "!트위치":
-        await message.channel.send("https://www.twitch.tv/cozy06242021")
-
-    if message.content == "!디코":
-        await message.channel.send("게임방 디스코드 방 : https://discord.gg/SFw3xdT")
-        await message.channel.send("추가예정 : 이유 : 올바르지 않은 초대장")
-
+@app.command()
+async def 사는곳(ctx):
+    await ctx.send('대한민국')
     
+@app.command()
+async def 방송계기(ctx):
+    await ctx.send('다른 스트리머가 게임하면서 방송하는게 멋있어서')
+@app.command()
+async def 트위치(ctx):
+    await ctx.send('https://www.twitch.tv/cozy06242021')
+
+@app.command()
+async def 유튜브(ctx):
+    await ctx.send('https://www.youtube.com/channel/UCXPOGWwjH-noqWCn_URbWGA')
+
+@app.command()
+async def 디코(ctx):
+    await ctx.send('게임방 디스코드 방 : https://discord.gg/SFw3xdT')
+    await ctx.send('트위치 디스코드 방 : https://discord.gg/tEEXa39')
+    await ctx.send('유튜브 디스코드 방 : https://discord.gg/wXTWKws')
+
 access_token = os.environ["BOT_TOKEN"]
-client.run(access_token)
+app.run(access_token)
